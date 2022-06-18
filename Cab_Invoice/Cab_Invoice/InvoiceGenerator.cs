8e5aa7d9256ce277079ed4770f16d51765d43b8a
Harinilekkala <harini.lekkala1997@gmail.com>
2022-06-18 16:06:42 +0530

    [Harini]: Calculate Fare

---
+++ Cab_Invoice/Cab_Invoice/InvoiceGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cab_Invoice
+{
+
+    public class InvoiceGenerator
+    {
+        private readonly double MINIMUM_COST_PER_KM = 10;
+        private readonly int COST_PER_TIME = 1;
+        private readonly double MINIMUM_FARE = 5;
+        public double distance;
+        public int time;
+        public InvoiceGenerator(double distance, int time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+        public double CalculateFare()
+        {
+            double totalFare = 0;
+            if (distance >= 0 && time >= 0)
+            {
+                totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+            }
+            else
+            {
+                if (distance <= 0)
+                {
+                    throw new CustomException(CustomException.ExceptionType.INVALID_DISTANCE, "Invalid Distance");
+                }
+                if (time < 0)
+                {
+                    throw new CustomException(CustomException.ExceptionType.INVALID_TIME, "Invalid Time");
+                }
+            }
+            return totalFare;
+        }
+    }
+}
+
---
+++ Cab_Invoice/Cab_Invoice/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Cab_Invoice;
+
+//InvoiceGenerator invoice = new InvoiceGenerator();
+
+using System;
+
+namespace CabInvoice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            InvoiceGenerator getMethod = new InvoiceGenerator(10.0, 5);
+            double fare = getMethod.CalculateFare();
+            Console.WriteLine("Fare: " + fare);
+        }
+    }
+}
